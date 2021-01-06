b3889df650b4eae2c93f757ade133a8ffd7e446c
arfadrohru <arfadrohru@gmail.com>
2021-01-06 22:03:23 +0700

    tambah user

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,48 @@
 import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
 
 function App () {
-    const [message, setMessage] = useState("");
-    const [message2, setMessage2] = useState("");
+    const [hello, setHello] = useState("hello");
+    const [hai, setHai] = useState("hai");
+    const [user, setUser] = useState([]);
 
     useEffect(() => {
-        fetch('api/hello')
-            .then(response => response.text())
-            .then(message => {
-                setMessage(message);
+        axios.get('api/hello')
+            .then(res => {
+                setHello(res.data)
             });
     },[])
 
     useEffect(() => {
-      fetch('api/hai')
-          .then(response => response.text())
-          .then(message => {
-              setMessage2(message);
+        axios.get('api/hai')
+          .then(res => {
+            setHai(res.data)
           });
   },[])
+
+  useEffect(() => {
+    axios.get('api/user')
+        .then(res => {
+            setUser(res.data.user)
+        });
+},[])
 
     return (
         <div className="App">
             <header className="App-header">
             <img src={logo} className="App-logo" alt="logo"/>
-            <h1 className="App-title">{message}</h1>
-            <h1 className="App-title">{message2}</h1>
+            <h1 className="App-title">{hello}</h1>
+            <h1 className="App-title">{hai}</h1>
+            <div style={{display:'flex', flexDirection:'row'}}>
+            {user.map((res,idx) =>{
+                return(
+                    <h1 style={{margin:'10px'}}>{res}</h1>
+                )
+            } )}
+            </div>
             </header>
-
         </div>
     )
 }
