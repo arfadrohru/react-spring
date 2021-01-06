38dbc1890305fc0076eb271935270db39ade22cb
arfadrohru <arfadrohru@gmail.com>
2021-01-06 20:17:54 +0700

    add username

--- src/main/java/com/example/reactspring/controllers/HomePage.java
+++ src/main/java/com/example/reactspring/controllers/HomePage.java
@@ -1,6 +1,10 @@
 package com.example.reactspring.controllers;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,5 +21,17 @@ public class HomePage {
 	 @GetMapping("/hai")
 	    public String hai() {
 	        return "Hai";
+	    }
+	 
+	 @GetMapping("/user")
+	    public Map<String, Object> user() {
+		 Map<String, Object> result = new HashMap<>();
+		 List<String> listUser = new ArrayList<>();
+		 listUser.add("Eizan");
+		 listUser.add("Imm");
+		 listUser.add("Reighn");
+		 listUser.add("Malkist");
+		 result.put("user", listUser);
+	        return result;
 	    }
 }
